78d2dc7cad5a5572cb24eb76406a7b21e2b04ae2
Tomás Sánchez <Tomassanchezmedina92@gmail.com>
2022-03-07 17:37:48 +0100

    chore: Iteration 1 improved

--- Iteration1.js
+++ Iteration1.js
@@ -1,24 +1,28 @@
 /* 1.1 Usa querySelector para mostrar por consola el botón con la clase .showme */
-console.log(document.querySelector('.showme'))
+ const showME = (document.querySelector('.showme'));
+ console.log(showME);
 
 /* 1.2 Usa querySelector para mostrar por consola el h1 con el id #pillado */
 
-console.log(document.querySelector('#pillado'))
+const pillado = document.querySelector('#pillado');
+console.log(pillado);
 
 /* 1.3 Usa querySelector para mostrar por consola todos los p */
-
-console.log(document.querySelectorAll('p'))
+const allParagraph = document.querySelectorAll('p');
+console.log(allParagraph);
 
 /* 1.4 Usa querySelector para mostrar por consola todos los elementos con la clase.pokemon */
 
-console.log(document.querySelectorAll('.pokemon'))
+const allPokemon = document.querySelectorAll('.pokemon')
+console.log(allPokemon);
 
 /* 1.5 Usa querySelector para mostrar por consola todos los elementos con el atributo 
 data-function="testMe". */
 
-console.log(document.querySelectorAll('[data-function="testMe"]'))
+const allData = document.querySelectorAll('[data-function="testMe"]')
+console.log(allData);
 
 /* 1.6 Usa querySelector para mostrar por consola el 3 personaje con el atributo 
 data-function="testMe". */
 
-/* console.log(document.querySelector('[data-function="testMe"] :nth-child()')) */
+console.log(allData[2]);
